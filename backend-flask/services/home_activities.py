3770efab92e6887ebe6c53feca1f5aaeed847cf0
Revathi Joshi <revathijoshi70@gmail.com>
2023-03-03 15:10:03 +0000

    updating honeycomb

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -6,11 +6,11 @@ tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run():
-   with tracer.start_as_current_span("home-activities-mock-data"):
-    span = trace.get_current_span()
-    now = datetime.now(timezone.utc).astimezone()
-    span.set_attribute("app.now", now.isoformat())
-    results = [{
+    with tracer.start_as_current_span("home-activities-mock-data"):
+      span = trace.get_current_span()
+      now = datetime.now(timezone.utc).astimezone()
+      span.set_attribute("app.now", now.isoformat())
+      results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
       'message': 'Cloud is very fun!',
