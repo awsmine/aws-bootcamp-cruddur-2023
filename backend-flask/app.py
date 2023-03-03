6e6412087942b087a59675ef19602f13af8cf866
Revathi Joshi <revathijoshi70@gmail.com>
2023-03-03 23:33:54 +0000

    configuring rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -27,6 +27,11 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+# import rollbar libraries
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # CloudWatch Logs -----
 # The strftime function is used to add timestamps to your log messages for easier analysis
 import watchtower
@@ -83,12 +88,36 @@ cors = CORS(
   methods="OPTIONS,GET,HEAD,POST"
 )
 
+# initializes and configures the Rollbar error tracking library
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
 # to collect logs for Cloudwatch errors 
 @app.after_request to collect logs for errors
 def after_request(response):
     timestamp = strftime('[%Y-%b-%d %H:%M]')
     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
     return response
+
+# add an endpoint just for testing rollbar
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"    
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
