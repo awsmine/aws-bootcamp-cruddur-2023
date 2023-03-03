b4833265aaa5f2c343497900ebf2ab7f096a66a4
Revathi Joshi <revathijoshi70@gmail.com>
2023-03-03 16:13:51 +0000

    adding honeycomb

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -9,7 +9,7 @@ class HomeActivities:
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat())
+      span.set_attribute("app.now",now.isoformat())
       results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
